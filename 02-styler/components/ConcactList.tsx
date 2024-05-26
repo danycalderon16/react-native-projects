a65db5ed7e4c3bc0733784dfd9d291f6c8b08b84
Daniel Calderon <danycalvir@gmail.com>
2024-05-25 22:40:43 -0700

    whatsapp contacts style

--- 02-styler/components/ConcactList.tsx
+++ 02-styler/components/ConcactList.tsx
@@ -35,8 +35,10 @@ const ConcactList = () => {
         {contacts.map(({uid, name, status, imageUrl}) => (
           <View key={uid} style={styles.userCard}>
             <Image source={{uri: imageUrl}} style={styles.userImage} />
-            <Text style={styles.userName}>{name}</Text>
-            <Text style={styles.userStatus}>{status}</Text>
+            <View>
+              <Text style={styles.userName}>{name}</Text>
+              <Text style={styles.userStatus}>{status}</Text>
+            </View>
           </View>
         ))}
       </ScrollView>
@@ -49,16 +51,34 @@ export default ConcactList;
 const styles = StyleSheet.create({
   headingText: {
     fontSize: 24,
-    paddingHorizontal: 8,
     fontWeight: 'bold',
+    paddingHorizontal: 8,
   },
-  container: {},
+  container: {
+    paddingHorizontal:16,
+    marginBottom:4
+  },
   userCard: {
+    flex:1,
+    flexDirection:'row',
+    alignItems:'center',
+    marginBottom:3,
+    backgroundColor:"#8d3daf",
+    padding:8,
+    borderRadius:10
   },
   userImage: {
     width: 60,
     height: 60,
-    borderRadius:60/2},
-  userName: {},
-  userStatus: {},
+    borderRadius:60/2,
+    marginRight:14
+  },
+  userName: {
+    fontSize: 16,
+    fontWeight: "600",
+    color:"#fff"
+  },
+  userStatus: {
+    fontSize:12,
+  },
 });
