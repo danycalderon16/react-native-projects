b193c38e365cd5e80ae8b1da81486f9330b03a9a
Daniel Calderon <danycalvir@gmail.com>
2024-05-25 22:16:46 -0700

    deeply link style

--- 02-styler/components/ActionCard.tsx
+++ 02-styler/components/ActionCard.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import { Image, Linking, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react';
+import {
+  Image,
+  Linking,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 export default function ActionCard() {
-
-  function openWebsite(websiteLink:string){
-    Linking.openURL(websiteLink)
+  function openWebsite(websiteLink: string) {
+    Linking.openURL(websiteLink);
   }
 
   return (
@@ -12,32 +18,39 @@ export default function ActionCard() {
       <Text style={styles.headingText}>Blog Card</Text>
       <View style={[styles.card, styles.elevatedCard]}>
         <View style={styles.headingContainer}>
-          <Text style={styles.headingText}>
+          <Text style={styles.headerText}>
             What's new in Javascript 21 - ES12
           </Text>
-          <Image
-            source={{
-              uri:"https://picsum.photos/400/200"
-            }}
-            style={styles.cardImage}
-          />
-          <View style={styles.bodyContainer}>
-            <Text numberOfLines={3}>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio ad delectus dolores porro culpa iusto asperiores sint dolore enim mollitia iure earum, dignissimos ipsa provident quidem nisi voluptatem rerum obcaecati?
-            </Text>
-          </View>
-          <View style={styles.footerContainer}>
-            <TouchableOpacity onPress={()=>openWebsite("https:www.github.com/danycalderon16")}>
-              <Text style={styles.socialLinks}>Read More</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={()=>openWebsite("https://www.linkedin.com/in/daniel-calderon-vi/")}>
-              <Text style={styles.socialLinks}>Follow me</Text>
-            </TouchableOpacity>
-          </View>
+        </View>
+        <Image
+          source={{
+            uri: 'https://picsum.photos/400/200',
+          }}
+          style={styles.cardImage}
+        />
+        <View style={styles.bodyContainer}>
+          <Text numberOfLines={3}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+            ad delectus dolores porro culpa iusto asperiores sint dolore enim
+            mollitia iure earum, dignissimos ipsa provident quidem nisi
+            voluptatem rerum obcaecati?
+          </Text>
+        </View>
+        <View style={styles.footerContainer}>
+          <TouchableOpacity
+            onPress={() => openWebsite('https:www.github.com/danycalderon16')}>
+            <Text style={styles.socialLinks}>Read More</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() =>
+              openWebsite('https://www.linkedin.com/in/daniel-calderon-vi/')
+            }>
+            <Text style={styles.socialLinks}>Follow me</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -46,25 +59,54 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     paddingHorizontal: 8,
   },
-  card:{
+  card: {
+    width: 'auto',
+    height: 360,
+    borderRadius: 6,
+    marginVertical: 12,
+    marginHorizontal: 16,
   },
-  elevatedCard:{
-    backgroundColor:"#cad5e2",
-    elevation:3,
-    shadowOffset:{
-      width:1,
-      height:1
+  elevatedCard: {
+    backgroundColor: '#e07c24',
+    elevation: 3,
+    shadowOffset: {
+      width: 1,
+      height: 1,
     },
-    shadowColor:"#ef5354",
-    shadowOpacity:0.4,
-    shadowRadius:2
+    shadowColor: '#333',
+    shadowOpacity: 0.4,
+    shadowRadius: 2,
   },
-  headingContainer:{
+  headingContainer: {
+    height:40,
+    flexDirection:"row",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  headerText: {
+    color:"#000",
+    fontSize:12,
+    fontWeight:"600"
+  },
+  cardImage: {
+    width: 'auto',
+    height: 190,
+  },
+  bodyContainer: {
+    padding:10,
+  },
+  footerContainer: {
     padding:8,
-    backgroundColor:"#cad5e2"
+    flexDirection:"row",
+    alignItems:"center",
+    justifyContent:"space-evenly"
   },
-  cardImage:{},
-  bodyContainer:{},
-  footerContainer:{},
-  socialLinks:{}
-})
+  socialLinks: {
+    fontSize:16,
+    color:"#000",
+    backgroundColor:"#fff",
+    paddingHorizontal:20,
+    paddingVertical:6,
+    borderRadius:6,
+  },
+});
